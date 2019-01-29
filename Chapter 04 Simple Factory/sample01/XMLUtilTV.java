fed7b7d8cd58e03895732b15eee922a060ed3a67
JRaiden <jraiden16@outlook.com>
2019-01-29 12:06:56 -0600

    Update XMLUtilTV.java

--- Chapter 04 Simple Factory/sample01/XMLUtilTV.java
+++ Chapter 04 Simple Factory/sample01/XMLUtilTV.java
@@ -4,18 +4,18 @@ import org.xml.sax.SAXException;
 import java.io.*;
 public class XMLUtilTV
 {
-    //该方法用于从XML配置文件中提取品牌名称，并返回该品牌名称
+    //Este m茅todo se utiliza para extraer la marca del archivo de configuraci贸n XML y devolver la marca
 	public static String getBrandName()
 	{
 		try
 		{
-			//创建文档对象
+			//Crear un objeto de documento
 			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dFactory.newDocumentBuilder();
 			Document doc;							
 			doc = builder.parse(new File("configTV.xml")); 
 		
-			//获取包含品牌名称的文本节点
+			//Obtener un nodo de texto que contiene la marca
 			NodeList nl = doc.getElementsByTagName("brandName");
             Node classNode=nl.item(0).getFirstChild();
             String brandName=classNode.getNodeValue().trim();
